850954236b66950733b6e44898502307b4f5964c
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-31 12:35:18 +0200

    add21

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'link-auth',
   templateUrl: './auth.component.html'
 })
 
-export class AuthComponent {
-
+export class AuthComponent implements  OnInit {
+    constructor(private router: Router) {
+    }
+    ngOnInit() {
+      this.router.navigate(['/login']);
+    }
 }
