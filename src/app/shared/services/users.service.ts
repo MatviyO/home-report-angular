41f61a51b95f6bba77b54ba791288506fda66c69
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-01-02 18:44:39 +0200

    add4355

---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+// @ts-ignore
+import { map } from 'rxjs/operatros';
+import {User} from '../models/user.model';
+
+@Injectable()
+export class UsersService {
+  constructor(private http: HttpClient) {
+  }
+
+  getUserByEmail(email: string): Observable<User> {
+    return this.http.get(`http://localhost:3000/users?email=${email}`).pipe(map( (response: Response) => response.json()));
+  }
+}
