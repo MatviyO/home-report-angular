88c5dc3e54146fa519801cb707b77a4fbeeabc7d
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-02-19 17:27:51 +0200

    add34PageNotFound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import {HttpModule} from '@angular/http';
 import {AuthService} from './shared/services/auth.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AuthGuard} from './shared/services/auth.guard';
+import {NotFoundComponent} from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
