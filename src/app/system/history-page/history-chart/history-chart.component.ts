e4e3439ff4a0767a52d1bf7b6b75b9f3926a1ada
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-02-19 10:07:55 +0200

    add27

--- src/app/system/history-page/history-chart/history-chart.component.ts
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input } from '@angular/core';
 @Component({
   selector: 'app-history-chart',
   templateUrl: './history-chart.component.html',
